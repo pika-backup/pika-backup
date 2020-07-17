6bad503c64a4c415fd3f62d42f04029ee8451e21
Sophie Herold <sophie@hemio.de>
2020-07-18 00:46:52 +0200

    Build details

--- build-aux/flatpak-build.bash
+++ build-aux/flatpak-build.bash
@@ -2,12 +2,12 @@
 
 APP_ID=$(cat data/APPLICATION_ID)
 REPO_NAME=pika-backup-dev
-REPO_DIR=flatpak_repo
+REPO_DIR=target/flatpak_repo
 
 flatpak remove $APP_ID -y
 flatpak-builder --install-deps-from=https://dl.flathub.org/repo/ \
   --user --verbose --force-clean -y --repo=$REPO_DIR flatpak_out build-aux/ci.manifest.yml
-flatpak build-bundle $REPO_DIR $APP_ID.flatpak $APP_ID
+flatpak build-bundle $REPO_DIR target/$APP_ID.flatpak $APP_ID
 flatpak --force remote-delete $REPO_NAME
 flatpak --user remote-add --no-gpg-verify $REPO_NAME $REPO_DIR
 flatpak --user install -y $REPO_NAME $APP_ID
