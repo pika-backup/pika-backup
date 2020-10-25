511086fd79b5fb9f569a52593af11cb673e79c07
Sophie Herold <sophie@hemio.de>
2020-10-25 22:19:05 +0100

    Switch to .json for flatpak devel manifest
    Closes #56

--- build-aux/generate-manifest-dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-./flatpak-builder-tools/cargo/flatpak-cargo-generator.py -o build-aux/generated-sources.json Cargo.lock
---
+++ build-aux/generate-manifest.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+
+cd "$(dirname "$0")/.."
+
+yq '
+    .modules[-1].sources[-1] = {type: "dir", path: ".."} |
+    .modules[-1]["config-opts"] = ["-Dprofile=dev"]' \
+    build-aux/org.gnome.World.PikaBackup.yml > build-aux/devel.manifest.json
+
+./flatpak-builder-tools/cargo/flatpak-cargo-generator.py \
+    -o build-aux/generated-sources.json Cargo.lock
