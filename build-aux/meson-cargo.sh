eeb237a1c02c6fb56def0aa1c6be508c259ce6ee
Fina Wilke <code@felinira.net>
2020-11-25 03:26:34 +0100

    Remove meson-cargo.sh and replace with a call to env and a cp target in meson

--- build-aux/meson-cargo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -x
-
-eval $*
-
-cargo build \
-    --manifest-path "${MESON_SOURCE_ROOT}/Cargo.toml" \
-    --target-dir "$CARGO_TARGET_DIR" \
-    $CARGO_OPTIONS && \
-cp "$CARGO_OUTPUT" "$OUTPUT"
