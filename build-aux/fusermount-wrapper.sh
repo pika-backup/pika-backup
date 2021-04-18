2c69a586e1c1651ad30729d376d478d2bb2eb7c2
Sophie Herold <sophie@hemio.de>
2021-04-18 14:52:39 +0200

    Fix "bad fd" warnings in fusermount-wrapper
    Closes #118

--- build-aux/fusermount-wrapper.sh
+++ build-aux/fusermount-wrapper.sh
@@ -6,4 +6,8 @@ else
     FD_ARGS="--env=_FUSE_COMMFD=${_FUSE_COMMFD} --forward-fd=${_FUSE_COMMFD}"
 fi
 
-exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 --forward-fd=3 $FD_ARGS fusermount "$@"
+if [ -e /proc/self/fd/3 ] && [ 3 != "$_FUSE_COMMFD" ]; then
+    FD_ARGS="$FD_ARGS --forward-fd=3"
+fi
+
+exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount "$@"
