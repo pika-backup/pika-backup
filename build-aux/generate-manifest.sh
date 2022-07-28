6427927fbbff02648beade7a7bbfd1feb1dc57f4
Sophie Herold <sophie@hemio.de>
2022-07-28 05:21:36 +0200

    flatpak: Use construction sign as devel suffix

--- build-aux/generate-manifest.sh
+++ build-aux/generate-manifest.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")/.."
 
 yq '
     .["app-id"] += ".Devel" |
-    .["desktop-file-name-suffix"] = " ☢" |
+    .["desktop-file-name-suffix"] = " 🚧" |
     .modules[-1].sources[0] = {type: "dir", path: ".."} |
     .modules[-1]["config-opts"] = ["-Dprofile=dev", "-Dapp_id_suffix=.Devel"]' \
     build-aux/org.gnome.World.PikaBackup.yml > build-aux/org.gnome.World.PikaBackup.Devel.json
