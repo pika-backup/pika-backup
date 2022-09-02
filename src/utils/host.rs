512ef0a1242fa9a0195f6ca9f97adb4f521712df
Sophie Herold <sophie@hemio.de>
2022-09-02 02:59:38 +0200

    exclude: Use correct xdg paths inside flatpak

--- src/utils/host.rs
+++ src/utils/host.rs
@@ -1,24 +1,34 @@
 use std::path::PathBuf;
 
-// TODO: The flatpak versions are wrong
-
 pub fn user_data_dir() -> PathBuf {
-    match std::env::var_os("HOST_XDG_DATA_HOME") {
-        Some(dir) if ashpd::is_sandboxed() => dir.into(),
-        _ => glib::user_data_dir(),
+    if ashpd::is_sandboxed() {
+        match std::env::var_os("HOST_XDG_DATA_HOME") {
+            Some(dir) if !dir.is_empty() => dir.into(),
+            _ => glib::home_dir().join(".local/share"),
+        }
+    } else {
+        glib::user_data_dir()
     }
 }
 
 pub fn user_cache_dir() -> PathBuf {
-    match std::env::var_os("HOST_XDG_CACHE_HOME") {
-        Some(dir) if ashpd::is_sandboxed() => dir.into(),
-        _ => glib::user_cache_dir(),
+    if ashpd::is_sandboxed() {
+        match std::env::var_os("HOST_XDG_CACHE_HOME") {
+            Some(dir) if !dir.is_empty() => dir.into(),
+            _ => glib::home_dir().join(".cache"),
+        }
+    } else {
+        glib::user_cache_dir()
     }
 }
 
 pub fn user_config_dir() -> PathBuf {
-    match std::env::var_os("HOST_XDG_CONFIG_HOME") {
-        Some(dir) if ashpd::is_sandboxed() => dir.into(),
-        _ => glib::user_config_dir(),
+    if ashpd::is_sandboxed() {
+        match std::env::var_os("HOST_XDG_CONFIG_HOME") {
+            Some(dir) if !dir.is_empty() => dir.into(),
+            _ => glib::home_dir().join(".config"),
+        }
+    } else {
+        glib::user_config_dir()
     }
 }
