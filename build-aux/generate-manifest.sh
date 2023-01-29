0f0fb1d7b79e302be98646dab3b2d5dc4738ae62
Sophie Herold <sophie@hemio.de>
2023-01-29 18:47:13 +0100

    cargo: Update deps, remove generated flatpak deps
    We don't build with the flatpak deps in CI anyways

--- build-aux/generate-manifest.sh
+++ build-aux/generate-manifest.sh
@@ -12,6 +12,3 @@ yq '
     .modules[-1].sources[0] = {type: "dir", path: ".."} |
     .modules[-1]["config-opts"] = ["-Dprofile=dev", "-Dapp_id_suffix=.Devel"]' \
     build-aux/org.gnome.World.PikaBackup.yml > build-aux/org.gnome.World.PikaBackup.Devel.json
-
-./build-aux/flatpak-builder-tools/cargo/flatpak-cargo-generator.py \
-    -o build-aux/generated-sources.json Cargo.lock
