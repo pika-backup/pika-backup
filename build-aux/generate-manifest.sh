0db08c9406cce495282cc4d93611391a7d73c5a6
Sophie Herold <sophie@hemio.de>
2023-02-18 19:47:51 +0100

    buildAux: Support newer yq version

--- build-aux/generate-manifest.sh
+++ build-aux/generate-manifest.sh
@@ -4,11 +4,10 @@ set -e
 
 cd "$(dirname "$0")/.."
 
-yq '
+yq -o json '
     .["app-id"] += ".Devel" |
     .["desktop-file-name-suffix"] = " 🚧" |
-    # For apps like Builder that do not run the last module
     .["build-options"] += {"build-args": ["--share=network"]} |
-    .modules[-1].sources = [{type: "dir", path: ".."}] |
+    .modules[-1].sources = [{"type": "dir", "path": ".."}] |
     .modules[-1]["config-opts"] = ["-Dprofile=dev", "-Dapp_id_suffix=.Devel"]' \
     build-aux/org.gnome.World.PikaBackup.yml > build-aux/org.gnome.World.PikaBackup.Devel.json
