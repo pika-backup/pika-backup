2b366ac87ab03680b5c94c1fd50b20e5fa0ff2e0
Fina Wilke <code@felinira.net>
2023-03-01 19:48:20 +0100

    manifest: Add debug logging to devel manifest

--- build-aux/generate-manifest.sh
+++ build-aux/generate-manifest.sh
@@ -8,6 +8,7 @@ yq -o json '
     .["app-id"] += ".Devel" |
     .["desktop-file-name-suffix"] = " 🚧" |
     .["build-options"] += {"build-args": ["--share=network"]} |
+    .["finish-args"] += "--env=G_MESSAGES_DEBUG=all" |
     .modules[-1].sources = [{"type": "dir", "path": ".."}] |
     .modules[-1]["config-opts"] = ["-Dprofile=dev", "-Dapp_id_suffix=.Devel"]' \
     build-aux/org.gnome.World.PikaBackup.yml > build-aux/org.gnome.World.PikaBackup.Devel.json
