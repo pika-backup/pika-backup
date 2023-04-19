35a6ae29c375d8ff2924edf58c69b44c2aacea4f
Fina Wilke <code@felinira.net>
2023-04-19 23:15:41 +0200

    fusermount: Fix running on non en_US locales

--- build-aux/fusermount-wrapper.sh
+++ build-aux/fusermount-wrapper.sh
@@ -10,18 +10,10 @@ if [ -e /proc/self/fd/3 ] && [ 3 != "$_FUSE_COMMFD" ]; then
     FD_ARGS="$FD_ARGS --forward-fd=3"
 fi
 
-# Ignore stdout, redirect stderr for further inspection
-STDERR="$(exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount "$@" 2>&1 > /dev/null)"
-RETURN=$?
-
-# If the fusermount binary doesn't exist we try fusermount3
-if [[ "$RETURN" -eq 1 && "$STDERR" == *"No such file or directory"* ]]; then
-    STDERR="$(exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount3 "$@" 2>&1 > /dev/null)"
-    RETURN=$?
+# If the fusermount3 binary doesn't exist we try fusermount
+# command -v will return 0 if the command exists and 127 otherwise
+if flatpak-spawn --host sh -c "command -v fusermount3" > /dev/null; then
+    exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount3 "$@"
+else
+    exec flatpak-spawn --host --forward-fd=1 --forward-fd=2 $FD_ARGS fusermount "$@"
 fi
-
-# Output stderr from the last fusermount call for further inspection
-echo -n "$STDERR" >&2
-
-# Return the return code of the last fusermount call
-exit $RETURN
