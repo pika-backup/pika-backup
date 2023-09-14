920bd20a24777263834d4dbd1a889049eb4876c8
Fina Wilke <code@felinira.net>
2023-09-14 19:44:37 +0200

    meson-cargo-manifest: Compatibility for python < 3.11
    This is required for the flathub build infrastructure

--- build-aux/meson-cargo-manifest.py
+++ build-aux/meson-cargo-manifest.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
-import tomllib
+try:
+    import tomllib
+except ImportError:
+    # Compatibility for Python < 3.11
+    from pip._vendor import tomli as tomllib
 import sys
 
 with open('Cargo.toml', 'rb') as f:
     toml = tomllib.load(f)
-    print(toml[sys.argv[1]][sys.argv[2]], end='')
+    for arg in sys.argv[1:]:
+        toml = toml[arg]
+    print(toml, end='')
