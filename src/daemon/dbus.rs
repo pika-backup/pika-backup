pika-backup/pika-backup

commit 49603abe37abef6148465f6eb3f03f9b7d783a76

zbus: Cache all proxy connections

src/daemon/dbus.rs
@@ -1,8 +1,8 @@
 use crate::daemon::prelude::*;
-
-use zbus::Result;
-
 use crate::schedule;
+
+use once_cell::sync::OnceCell;
+use zbus::Result;
 
 #[zbus::dbus_proxy(interface = "org.gnome.World.PikaBackup1", assume_defaults = false)]
 trait PikaBackup {
@@ -23,11 +23,18 @@ pub struct PikaBackup;
 
 impl PikaBackup {
     pub async fn proxy() -> Result<PikaBackupProxy<'static>> {
-        PikaBackupProxy::builder(&ZBUS_SESSION)
-            .destination(crate::DBUS_API_NAME)?
-            .path(crate::DBUS_API_PATH)?
-            .build()
-            .await
+        static PROXY: once_cell::sync::OnceCell<PikaBackupProxy<'static>> = OnceCell::new();
+
+        if let Some(proxy) = PROXY.get() {
+            Ok(proxy.clone())
+        } else {
+            let proxy = PikaBackupProxy::builder(&crate::utils::dbus::system().await?)
+                .destination(crate::DBUS_API_NAME)?
+                .path(crate::DBUS_API_PATH)?
+                .build()
+                .await?;
+            Ok(PROXY.get_or_init(move || proxy).clone())
+        }
     }
 
     pub async fn start_scheduled_backup(

src/utils/dbus.rs
@@ -1,0 +1,37 @@
+use once_cell::sync::OnceCell;
+
+/// Session Bus
+pub async fn session() -> Result<zbus::Connection, zbus::Error> {
+    static CONNECTION: OnceCell<zbus::Connection> = OnceCell::new();
+
+    if let Some(connection) = CONNECTION.get() {
+        Ok(connection.clone())
+    } else {
+        let connection = zbus::Connection::session().await?;
+        Ok(CONNECTION.get_or_init(move || connection).clone())
+    }
+}
+
+/// System Bus
+pub async fn system() -> Result<zbus::Connection, zbus::Error> {
+    static CONNECTION: OnceCell<zbus::Connection> = OnceCell::new();
+
+    if let Some(connection) = CONNECTION.get() {
+        Ok(connection.clone())
+    } else {
+        let connection = zbus::Connection::system().await?;
+        Ok(CONNECTION.get_or_init(move || connection).clone())
+    }
+}
+
+/// FDO proxy
+pub async fn fdo_proxy() -> Result<zbus::fdo::DBusProxy<'static>, zbus::Error> {
+    static PROXY: OnceCell<zbus::fdo::DBusProxy<'static>> = OnceCell::new();
+
+    if let Some(proxy) = PROXY.get() {
+        Ok(proxy.clone())
+    } else {
+        let proxy = zbus::fdo::DBusProxy::new(&crate::utils::dbus::system().await?).await?;
+        Ok(PROXY.get_or_init(move || proxy).clone())
+    }
+}

src/ui/dbus.rs
@@ -3,7 +3,6 @@ use async_std::prelude::*;
 
 use crate::{schedule, ui};
 use async_std::channel::Sender;
-use once_cell::sync::Lazy;
 
 struct PikaBackup {
     command: Sender<Command>,
@@ -57,9 +56,7 @@ impl PikaBackup {
     }
 }
 
-pub fn init() {
-    Lazy::force(&ZBUS_SESSION);
-
+pub async fn init() {
     let (sender, mut receiver) = async_std::channel::unbounded();
 
     Handler::run(async move {
@@ -83,10 +80,11 @@ pub fn init() {
 }
 
 async fn spawn_server(command: Sender<Command>) -> zbus::Result<()> {
-    ZBUS_SESSION
+    let zbus_session = crate::utils::dbus::session().await?;
+    zbus_session
         .object_server()
         .at(crate::DBUS_API_PATH, PikaBackup { command })
         .await?;
 
-    ZBUS_SESSION.request_name(crate::DBUS_API_NAME).await
+    zbus_session.request_name(crate::DBUS_API_NAME).await
 }

po/POTFILES.in
@@ -139,6 +139,7 @@ src/ui/widget/status_icon.rs
 src/ui/widget/status_row.rs
 src/ui/widget/wrap_box.rs
 src/utils.rs
+src/utils/dbus.rs
 src/utils/host.rs
 src/utils/password.rs
 src/utils/upower.rs

src/globals.rs
@@ -4,24 +4,6 @@ use once_cell::sync::OnceCell;
 use crate::config;
 use crate::prelude::*;
 
-/// Session Bus
-pub static ZBUS_SESSION: Lazy<zbus::Connection> = Lazy::new(|| {
-    async_std::task::block_on(async {
-        zbus::Connection::session()
-            .await
-            .expect("Failed to create ZBus session connection.")
-    })
-});
-
-/// System Bus
-pub static ZBUS_SYSTEM: Lazy<zbus::Connection> = Lazy::new(|| {
-    async_std::task::block_on(async {
-        zbus::Connection::system()
-            .await
-            .expect("Failed to create ZBus system connection.")
-    })
-});
-
 pub static LIB_USER: OnceCell<LibUser> = OnceCell::new();
 
 pub static APP_IS_SANDBOXED: Lazy<bool> =

src/ui.rs
@@ -126,7 +126,9 @@ fn on_startup(_app: &adw::Application) {
     }
 
     ui::actions::init();
-    ui::dbus::init();
+    glib::MainContext::default().spawn_local(async {
+        ui::dbus::init().await;
+    });
 
     ui::app_window::init();
     ui::headerbar::init();

src/ui/shell.rs
@@ -1,23 +1,21 @@
 use super::prelude::*;
 use arc_swap::ArcSwap;
 
-static BACKGROUND_PROXY: once_cell::sync::OnceCell<
-    Arc<ashpd::desktop::background::BackgroundProxy<'static>>,
-> = once_cell::sync::OnceCell::new();
 static LAST_MESSAGE: once_cell::sync::Lazy<ArcSwap<Option<String>>> =
     once_cell::sync::Lazy::new(|| ArcSwap::new(Default::default()));
 
 async fn proxy() -> Option<Arc<ashpd::desktop::background::BackgroundProxy<'static>>> {
-    match BACKGROUND_PROXY.get() {
-        Some(proxy) => Some(proxy.clone()),
-        None => ashpd::desktop::background::BackgroundProxy::new()
+    static PROXY: once_cell::sync::OnceCell<
+        Arc<ashpd::desktop::background::BackgroundProxy<'static>>,
+    > = once_cell::sync::OnceCell::new();
+
+    if let Some(proxy) = PROXY.get() {
+        Some(proxy.clone())
+    } else {
+        ashpd::desktop::background::BackgroundProxy::new()
             .await
             .ok()
-            .map(|proxy| {
-                BACKGROUND_PROXY
-                    .get_or_init(move || Arc::new(proxy))
-                    .clone()
-            }),
+            .map(|proxy| PROXY.get_or_init(move || Arc::new(proxy)).clone())
     }
 }
 

src/ui/utils.rs
@@ -13,6 +13,7 @@ use adw::prelude::*;
 use crate::config;
 
 use ashpd::desktop::background;
+use std::convert::TryInto;
 use std::fmt::Display;
 use std::io::Read;
 use std::os::unix::process::CommandExt;
@@ -99,17 +100,15 @@ pub fn cache_dir() -> std::path::PathBuf {
         .iter()
         .collect()
 }
-use std::convert::TryInto;
+
 pub async fn background_permission() -> Result<()> {
     let generic_msg = gettext("Request to run in background failed");
 
     if !*crate::globals::APP_IS_SANDBOXED {
         // start
-        let proxy = zbus::fdo::DBusProxy::new(&ZBUS_SESSION)
+        crate::utils::dbus::fdo_proxy()
             .await
-            .err_to_msg(&generic_msg)?;
-
-        proxy
+            .err_to_msg(&generic_msg)?
             .start_service_by_name(crate::DAEMON_APP_ID.try_into().unwrap(), Default::default())
             .await
             .err_to_msg(&generic_msg)?;

commit fa159ce1411431fc9cee28fb682d5a916a42fc01

dbus: Use session bus for FDO and Pika Api
Regression from 49603ab. Fixes scheduled backups not working.

src/daemon/dbus.rs
@@ -28,7 +28,7 @@ impl PikaBackup {
         if let Some(proxy) = PROXY.get() {
             Ok(proxy.clone())
         } else {
-            let proxy = PikaBackupProxy::builder(&crate::utils::dbus::system().await?)
+            let proxy = PikaBackupProxy::builder(&crate::utils::dbus::session().await?)
                 .destination(crate::DBUS_API_NAME)?
                 .path(crate::DBUS_API_PATH)?
                 .build()

src/utils/dbus.rs
@@ -31,7 +31,7 @@ pub async fn fdo_proxy() -> Result<zbus::fdo::DBusProxy<'static>, zbus::Error> {
     if let Some(proxy) = PROXY.get() {
         Ok(proxy.clone())
     } else {
-        let proxy = zbus::fdo::DBusProxy::new(&crate::utils::dbus::system().await?).await?;
+        let proxy = zbus::fdo::DBusProxy::new(&crate::utils::dbus::session().await?).await?;
         Ok(PROXY.get_or_init(move || proxy).clone())
     }
 }