911b735310ab09de33ed7ef3161534113c14c90a
Jonathan Lebon <jonathan@jlebon.com>
2024-03-23 16:57:17 -0400

    archives_page: respect 12h/24h setting when displaying timestamps
    The `%c` format specifier for GDateTime's `format` is only based on the
    current locale and does not account for whether the user has customized
    the clock locally to use 24h or 12h formatting.
    
    Fix this by querying the `clock-format` GSetting key using the Settings
    portal and customizing the timestamp format based on that.
    
    I made the querying a global lazy closure based on other similar code.
    This does mean that once the app is started, we won't query it again.
    OTOH, it's not common to routinely change the clock setting, so it
    didn't seem worth the complexity of e.g. monitoring for changes.
    
    The time formats encoded here match what is used by gnome-desktop itself
    when rendering the wall clock.
    
    Closes: #463

--- src/globals.rs
+++ src/globals.rs
@@ -4,10 +4,23 @@ use once_cell::sync::OnceCell;
 use crate::config;
 use crate::prelude::*;
 
+const CLOCK_INTERFACE: &str = "org.gnome.desktop.interface";
+const CLOCK_KEY: &str = "clock-format";
+
 pub static LIB_USER: OnceCell<LibUser> = OnceCell::new();
 
 pub static APP_IS_SANDBOXED: Lazy<bool> =
     Lazy::new(|| async_std::task::block_on(ashpd::is_sandboxed()));
+
+pub static CLOCK_IS_24H: Lazy<bool> = Lazy::new(|| {
+    async_std::task::block_on(async {
+        let proxy = ashpd::desktop::settings::Settings::new().await?;
+        proxy.read::<String>(CLOCK_INTERFACE, CLOCK_KEY).await
+    })
+    .map(|s| s == "24h")
+    .inspect_err(|e| warn!("Retrieving '{}' setting failed: {}", CLOCK_KEY, e))
+    .unwrap_or_default()
+});
 
 pub static MEMORY_PASSWORD_STORE: Lazy<
     std::sync::Arc<crate::utils::password::MemoryPasswordStore>,
