3f5d17aab28c74e68b7b4cfdc50ec8e9c5cea3ed
Fina Wilke <code@felinira.net>
2024-06-27 23:45:39 +0200

    setup: Implement centered dialog page design

--- src/ui/widget/dialog_page.rs
+++ src/ui/widget/dialog_page.rs
@@ -2,14 +2,30 @@ use adw::prelude::*;
 use adw::subclass::prelude::*;
 use glib::WeakRef;
 
+pub use imp::DialogPagePropertiesExt;
+
 mod imp {
+    use std::cell::{Cell, RefCell};
+
     use super::*;
 
-    #[derive(Default, glib::Properties)]
-    #[properties(wrapper_type = super::DialogPage)]
+    #[derive(Default, glib::Properties, gtk::CompositeTemplate)]
+    #[properties(wrapper_type = super::DialogPage, ext_trait)]
+    #[template(file = "dialog_page.ui")]
     pub struct DialogPage {
         #[property(get, set, nullable)]
         default_widget: WeakRef<gtk::Widget>,
+        #[property(get, set = Self::set_subtitle, nullable)]
+        subtitle: RefCell<Option<String>>,
+
+        #[property(get, set)]
+        show_continue_button: Cell<bool>,
+        #[template_child]
+        toolbar_view: TemplateChild<adw::ToolbarView>,
+        #[template_child]
+        content_box: TemplateChild<gtk::Box>,
+        #[template_child]
+        subtitle_label: TemplateChild<gtk::Label>,
     }
 
     #[glib::object_subclass]
@@ -17,16 +33,38 @@ mod imp {
         const NAME: &'static str = "PkDialogPage";
         type Type = super::DialogPage;
         type ParentType = adw::NavigationPage;
+        type Interfaces = (gtk::Buildable,);
+
+        fn class_init(klass: &mut Self::Class) {
+            klass.bind_template();
+        }
+
+        fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+            obj.init_template();
+        }
     }
 
     #[glib::derived_properties]
     impl ObjectImpl for DialogPage {}
     impl WidgetImpl for DialogPage {}
     impl NavigationPageImpl for DialogPage {}
-    impl PkDialogPageImpl for DialogPage {}
+    impl BuildableImpl for DialogPage {
+        fn add_child(&self, builder: &gtk::Builder, child: &glib::Object, type_: Option<&str>) {
+            if let Some(widget) = child.downcast_ref::<gtk::Widget>() {
+                self.content_box.append(widget);
+            } else {
+                self.parent_add_child(builder, child, type_);
+            }
+        }
+    }
 
-    #[gtk::template_callbacks]
-    impl DialogPage {}
+    impl DialogPage {
+        fn set_subtitle(&self, subtitle: Option<String>) {
+            self.subtitle_label
+                .set_visible(subtitle.as_ref().is_some_and(|s| !s.is_empty()));
+            self.subtitle.replace(subtitle);
+        }
+    }
 }
 
 glib::wrapper! {
@@ -41,14 +79,5 @@ pub trait PkDialogPageImpl: NavigationPageImpl + WidgetImpl + ObjectImpl {}
 unsafe impl<T: PkDialogPageImpl> IsSubclassable<T> for DialogPage {}
 
 #[allow(dead_code)]
-pub trait DialogPageExt: IsA<DialogPage> {
-    fn set_default_widget(&self, widget: Option<impl IsA<gtk::Widget>>) {
-        <self::DialogPage>::set_default_widget(self.upcast_ref::<DialogPage>(), widget);
-    }
-
-    fn default_widget(&self) -> Option<gtk::Widget> {
-        <self::DialogPage>::default_widget(self.upcast_ref::<DialogPage>())
-    }
-}
-
+pub trait DialogPageExt: IsA<DialogPage> + imp::DialogPagePropertiesExt {}
 impl<T: IsA<DialogPage>> DialogPageExt for T {}
