d85de906342d33fa0092677eb7618c8d31b3dda5
Fina Wilke <code@felinira.net>
2024-06-29 16:23:51 +0200

    widget: Add SpinnerPage

--- src/ui/widget/navigation.rs
+++ src/ui/widget/navigation.rs
@@ -1,9 +1,12 @@
 mod dialog_page;
+mod spinner_page;
 
 pub use dialog_page::{DialogPage, DialogPagePropertiesExt, PkDialogPageImpl};
+pub use spinner_page::{PkSpinnerPageImpl, SpinnerPage, SpinnerPagePropertiesExt};
 
 use glib::types::StaticTypeExt;
 
 pub fn init() {
     DialogPage::ensure_type();
+    SpinnerPage::ensure_type();
 }
