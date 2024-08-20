f192b574631d4cf5da8ce3402d002a409f373bf7
Sophie Herold <sophie@hemio.de>
2024-08-20 13:08:49 +0200

    code: Allow clippy::mutable_key_type
    It gives false positives for Pattern that contains Regex which seems to
    have interior mutability according to 1.80. Since we only use the regex
    string for Eq and Ord, this should not be a problem for HashSets etc.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![allow(clippy::derive_partial_eq_without_eq)]
 #![allow(clippy::get_first)]
 #![allow(clippy::new_without_default)]
+// Pattern contains a Regex which apparently has some interior mutability.
+// That should however not influence our Ord and Eq implementations.
+#![allow(clippy::mutable_key_type)]
 
 #[macro_use]
 extern crate serde_derive;
